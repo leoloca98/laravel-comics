2280dccc2f6d817c4247192aa327d99335d611a4
leoloca98 <leonardolocatelli98@gmail.com>
2021-10-01 18:06:26 +0200

    Add Footer-Bg + Footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -112,6 +112,146 @@
         </div>
     </main>
 
+    <footer>
+        <div id="jumbotron">
+            <div class="container">
+                <div class="row">
+                    <!-- Links -->
+                    <div class="col">
+                        <div class="row-between">
+                            <div class="col-3">
+                                <!-- Vertical Column -->
+                                <div class="row-column">
+                                    <div class="col-8">
+                                        <h2>DC COMICS</h2>
+                                        <ul>
+                                            <li>
+                                                <a href="#">Characters</a>
+                                            </li>
+                                            <li>
+                                                <a href="#">Comics</a>
+                                            </li>
+                                            <li>
+                                                <a href="#">Movies</a>
+                                            </li>
+                                            <li>
+                                                <a href="#">Tv</a>
+                                            </li>
+                                            <li>
+                                                <a href="#">Games</a>
+                                            </li>
+                                            <li>
+                                                <a href="#">Videos</a>
+                                            </li>
+                                            <li>
+                                                <a href="#">News</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-4">
+                                        <h2>SHOP</h2>
+                                        <ul>
+                                            <li>
+                                                <a href="#">Shop DC</a>
+                                            </li>
+                                            <li>
+                                                <a href="#">Shop DC Collectibles</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-3">
+                                <!-- DC Column -->
+                                <h2>DC</h2>
+                                <ul>
+                                    <li>
+                                        <a href="#">Term Of Use</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Privacy policy (new)</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Ad Choices</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Advertising</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Jobs</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Subscriptions</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Talent Workshop</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">CPSC Certificate</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Ratings</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Shop Help</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Contact Us</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="col-3">
+                                <!-- Sites Column -->
+                                <h2>SITES</h2>
+                                <ul>
+                                    <li>
+                                        <a href="#">DC</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">MAD Magazine</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">DC Kids</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">DC Universe</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">DC Power Visa</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Logo Dc -->
+                    <div class="col-logo">
+                        <img src="{{ asset('images/dc-logo-bg.png') }}" />
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div id="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-1">
+                        <input class="button" type="button" value="SIGN UP NOW!" />
+                    </div>
+                    <div class="col-2">
+                        <div class="text-blue">FOLLOW US</div>
+                        {{-- <ul>
+                            <li v-for="(socialIcon, index) in socialIcons" :key="index">
+                                <a href="#" class="social"><img :src="socialIcon.image"
+                                        :alt="socialIcon.name" /></a>
+                            </li>
+                        </ul> --}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
 
